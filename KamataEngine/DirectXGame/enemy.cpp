2712f76b429f663657e627fe023a73123910ca80
hirotosakurai <k021g1130@g.neec.ac.jp>
2022-10-20 11:52:08 +0900

    完成

--- KamataEngine/DirectXGame/enemy.cpp
+++ KamataEngine/DirectXGame/enemy.cpp
@@ -4,22 +4,23 @@
 MatWorld* enemyMatworld = nullptr;
 
 void Enemy::Initialize(Model* model, const Vector3& position) {
+
 	//NULLポインタチェック
 	assert(model);
 
 	model_ = model;
 
 	//テクスチャ読み込み
-	textureHandle_ = TextureManager::Load("Enemy.jpg");
+	textureHandle_ = TextureManager::Load("Enemy.png");
 
 	//ワールド変換の初期化
 	worldTransform_.Initialize();
 
 	//キャラクターの移動ベクトル
-	Vector3 move = { 0,20,0 }; //座標{x,y,z}
+	Vector3 move = { 0,10,0 }; //座標{x,y,z}
 
 	//初期座標をセット
-	worldTransform_.translation_ = move;
+	worldTransform_.translation_ = position;
 }
 
 Vector3 Enemy::GetWorldPosition()
@@ -52,14 +53,14 @@ void Enemy::Update()
 //衝突判定
 void Enemy::OnCollision()
 {
-	shootFlag = 1;
+	isDead_ = true;
 }
 
 void Enemy::Draw(ViewProjection& viewProjection)
 {
-	if (shootFlag == 0)
-	{
+	//if (flag == 0)
+	//{
 		//モデルの描画
-		model_->Draw(worldTransform_, viewProjection, textureHandle_);
-	}
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+	//}
 }
